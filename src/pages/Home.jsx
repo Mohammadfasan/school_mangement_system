e8798dfdb1567ede95202010d631e74f729f47af
Mohammadfasan <mohammedfasan617@gmail.com>
2025-10-06 19:40:42 +0530

    Add feature section with card styling, borders, and hover effects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Feature from '../components/Feature'
 
 const Home = () => {
   return (
     <div className="min-h-screen bg-white">
       <Hero />
+      <Feature /> 
     </div>
   )
 }
